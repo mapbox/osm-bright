586fc398f4f062d4d505be0f9426c60accf37d58
AJ Ashton <aj@developmentseed.org>
2012-02-08 15:56:35 -0500

    Aerodrome typo

--- imposm-mapping.py
+++ imposm-mapping.py
@@ -304,7 +304,7 @@ transport_areas = Polygons(
             'station',
         ),
         'aeroway': (
-            'aerodome',
+            'aerodrome',
             'terminal',
             'helipad',
             'apron',
