d83fd74694e333e3cce9b4eeb3f1e998f0297607
Aldo Culquicondor <alculquicondor@gmail.com>
2016-10-31 21:52:25 -0500

    building mapnik file on image creation
    also simplifying server code

---
+++ docker/tile_server/configure.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from os import path, getcwd
+from collections import defaultdict
+config = defaultdict(defaultdict)
+
+config["importer"] = "osm2pgsql" # either 'imposm' or 'osm2pgsql'
+
+# The name given to the style. This is the name it will have in the TileMill
+# project list, and a sanitized version will be used as the directory name
+# in which the project is stored
+config["name"] = "OSM Bright"
+
+# The absolute path to your MapBox projects directory. You should
+# not need to change this unless you have configured TileMill specially
+config["path"] = ""
+
+# PostGIS connection setup
+# Leave empty for Mapnik defaults. The only required parameter is dbname.
+config["postgis"]["host"]     = "postgis"
+config["postgis"]["port"]     = ""
+config["postgis"]["dbname"]   = "osm"
+config["postgis"]["user"]     = "osm"
+config["postgis"]["password"] = ""
+
+# Increase performance if you are only rendering a particular area by
+# specifying a bounding box to restrict queries. Format is "XMIN,YMIN,XMAX,YMAX"
+# in the same units as the database (probably spherical mercator meters). The
+# whole world is "-20037508.34 -20037508.34 20037508.34 20037508.34".
+# Leave blank to let Mapnik estimate.
+config["postgis"]["extent"] = "-20037508.34,-20037508.34,20037508.34,20037508.34"
+
+# Land shapefiles required for the style. If you have already downloaded
+# these or wish to use different versions, specify their paths here.
+# You will need to unzip these files before running make.py
+# These OSM land shapefiles are updated daily and can be downloaded at: 
+# - http://data.openstreetmapdata.com/simplified-land-polygons-complete-3857.zip
+# - http://data.openstreetmapdata.com/land-polygons-split-3857.zip
+
+config["land-high"] = "shp/land-polygons-split-3857/land_polygons.shp"
+config["land-low"] = "shp/simplified-land-polygons-complete-3857/simplified_land_polygons.shp"
+
+# Places shapefile required for the osm2pgsql style
+# - http://mapbox-geodata.s3.amazonaws.com/natural-earth-1.4.0/cultural/10m-populated-places-simple.zip
+#  or
+# - http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places.zip
+
+config["ne_places"] = "shp/10m-populated-places/10m-populated-places-simple.shp"
--- docker/tile_server/osm.py
+++ docker/tile_server/osm.py
@@ -1,35 +1,22 @@
 #!/usr/bin/env python
-from cStringIO import StringIO
 
-from flask import Flask, send_file
-from ModestMaps.Core import Coordinate
-from TileStache import parseConfig, getTile
+from TileStache import WSGITileServer
 
 
-app = Flask(__name__)
-
-config = parseConfig({
+config = {
     'cache': {
         'name': 'Disk',
         'path': 'tile_cache',
         'umask': '0000'
     },
     'layers': {
-        'osm': {
-            'provider': {'name': 'mapnik', 'mapfile': 'osm.xml'},
+        'osm_3857': {
+            'provider': {'name': 'mapnik', 'mapfile': 'osm_3857.xml'},
             'projection': 'spherical mercator',
-            'metatile': {'rows': 2, 'columns': 2, 'buffer': 64}
+            'metatile': {'rows': 2, 'columns': 2, 'buffer': 128}
         }
     }
-})
+}
 
 
-@app.route('/<int:z>/<int:x>/<int:y>.png')
-def base_layer(z, x, y):
-    content_type, tile = getTile(config.layers['osm'], Coordinate(y, x, z),
-                                 'png')
-    return send_file(StringIO(tile), mimetype=content_type)
-
-
-if __name__ == '__main__':
-    app.run()
+app = WSGITileServer(config=config, autoreload=True)
